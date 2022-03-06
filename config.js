2018aa0052f395a6f576067ac67d380762aff85b
truong <bokrinak@gmail.com>
2022-03-06 17:58:49 +0700

    baonganxinh

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Bé Đẹp';
+const giftUrl = 'https://www.facebook.com/nguyenvantruong310/';
+const eventName = 'Chúc Bé 8/3 Vui Vẻ';
+const titleCard = 'Lươn Trà Bảo Dỗi';
+const contentCard = 'Chúc bé của anh 8/3 tràn ngập niềm vui. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
